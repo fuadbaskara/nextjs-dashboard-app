e5e6f7b3841e830725aa90309d0fcb22d3e8eb36
fuadpayfazz <fuad.baskara@fazzfinancial.com>
2022-09-25 17:01:30 +0700

    feat(Tests): add some new test case, Button component and UserList snapshots

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,25 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'plugin:react/recommended'],
-  overrides: [],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+  ],
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.eslint.json',
   },
-  plugins: ['react'],
+  plugins: ['testing-library', 'prettier'],
   rules: {
     'react/function-component-definition': [
       2,
@@ -24,6 +35,7 @@ module.exports = {
     'arrow-body-style': 'off',
     'react/jsx-props-no-spreading': 'off',
     'no-param-reassign': 'off',
+    'implicit-arrow-linebreak': 'off',
     '@typescript-eslint/indent': 'off',
     'import/extensions': 'off',
     'object-curly-newline': 'off',
