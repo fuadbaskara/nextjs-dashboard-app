fc5d320a7e5f952f1b9dc5397808ae9a44eeb7a7
fuadbaskara <30852585+fuadbaskara@users.noreply.github.com>
2022-09-27 09:04:10 +0700

    fix(seo): add description meta, add name to pagination btn, update snapshots (#33)
    Co-authored-by: fuadpayfazz <fuad.baskara@fazzfinancial.com>

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import * as React from 'react';
 import Header from './header';
 
@@ -8,6 +9,14 @@ export type LayoutProps = {
 function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <title>Dashboard - Personal Project</title>
+        <meta
+          name="description"
+          content="Simple Dashboard app build with Nextjs + Typescript + @Redux/toolkit + tailwind and some more."
+        />
+        <meta name="robots" content="max-image-preview:large" />
+      </Head>
       <Header />
       <div className="mt-20 block bg-slate-100">
         <div className="flex w-full flex-1 flex-col">
